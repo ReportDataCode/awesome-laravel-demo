29a8ab5b4b1e615fae660e51d726fa537a8b68db
ReportDataCode <andrew-engineer@DESKTOP-H1I97F1.localdomain>
2021-10-05 06:22:16 +0800

    Implement a proof of concept payment gateway using Stripe's API

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    You're logged in!, Below are my proof of concepts in different APIs and technologies
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="https://buy.stripe.com/test_eVacMZcDq9Og1Py7st"> Buy me a Taho<a/>
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="https://buy.stripe.com/test_eVacMZcDq9Og1Py7st"> Map Navigation Using Azure Map API<a/>
                 </div>
             </div>
         </div>
